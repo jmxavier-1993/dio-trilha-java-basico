2fb0f7460f68ccf089e63d25eaa0dd0afded87ae
jmxavier-1993 <juliana.marinhoppf5@hotmail.com>
2024-06-26 20:19:20 -0300

    update tipoVariaveis

--- meu_primeiro_projeto_java/src/edu/juliana/Tiposevariaveis/TipoVariaveis.java
+++ meu_primeiro_projeto_java/src/edu/juliana/Tiposevariaveis/TipoVariaveis.java
@@ -1,6 +1,21 @@
 package edu.juliana.Tiposevariaveis;
+
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinino = 2500;
+        byte idade = 123;
+        short ano = 2024;
+        int cep= 53441625;// se começar com zero talvez precise que seja outro tipo
+        long cpf= 12556378912L; // se começar com zero talvez precise que seja outro tipo   
+        float pi =3.14F;
+
+        short numeroCurto=1;
+        int numeroNormal= numeroCurto;
+        short numeroCurto2= (short) numeroNormal;
+
+        final int NUMERO=5;
+        // numero=10;
+        System.out.println(NUMERO); // para tornar uma variavel de valor fixo "constante" é só usar o final e a variavel em caixa alta
+        
     }
 }
