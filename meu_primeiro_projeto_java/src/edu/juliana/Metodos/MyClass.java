54325b12f387b7c361ef205ad311f429d7330cb2
Juliana Marinho Xavier <142364762+jmxavier-1993@users.noreply.github.com>
2024-07-06 15:23:10 -0300

    Update aula metodos

---
+++ meu_primeiro_projeto_java/src/edu/juliana/Metodos/MyClass.java
@@ -0,0 +1,39 @@
+package edu.juliana.Metodos;
+
+public class MyClass {
+    
+
+        num1= 2;
+        num2= 3;
+
+        public double somar(int num1, int num2);
+        return (num1 + num2);
+
+        System.out.println(somar);
+
+        public void imprimir("teste");
+
+        dividendo=5;
+        divisor=2;
+
+        public double dividir (int dividendo, int divisor);
+        return (dividendo/divisor);
+
+        private void metodoPrivado(){}
+        // este metodo deveria retornar algum valor
+        // no caso boolean true or false
+
+        
+
+        private void validar(){
+
+        }
+
+        public void gravarCliente(String nome, String cpf,int idade);
+
+        return (nome+ cpf+ idade);
+
+        
+
+
+    }
